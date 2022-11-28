47f3dd9bda3ca289e74bdccb20eca179c069c0a5
Tomasz Żebrowski <zebromek@gmail.com>
2022-11-28 19:24:00 +0100

    Usuwam smieci, i gitara

--- gradient_descent.py
+++
@@ -1,6 +0,0 @@
-def gradient_descent(x, df, h=1e-4, e=1e-4, max_iters=1e6):
-    iters = 0
-    while (iters < max_iters and sum([i**2 for i in df(x)]) > e):
-        x = [x[i] - h * df(x)[i] for i in range(len(x))]
-        iters += 1
-    return x, iters
--- test.py
+++
@@ -1,4 +0,0 @@
-from gradient_descent import gradient_descent
-from functions import f, df, dg
-
-print(gradient_descent([1, 1], dg, 1e-5, 1e-8, 1e8))
