08cac58f913399ecc805d947b0a685c8a0be865d
tototmek <zebromek@gmail.com>
2022-12-22 17:00:52 +0100

    Komentarze do kodu

--- mlp.py
+++ mlp.py
@@ -8,7 +8,6 @@ def sigmoid(x):
 def d_sigmoid_dt(x):
     return sigmoid(x) * (1 - sigmoid(x))
 
-
 def relu(x):
     return (0 if x < 0 else x)
 
@@ -30,6 +29,7 @@ def d_elu_dt(x):
 def neuron(inputs, weights, bias, activation):
     return activation(sum([i * w for i, w in zip(inputs, weights)]) + bias)
 
+# Klasa reprezentująca perceptron wielowarstwowy
 class MLP:
     def __init__(self, input_size, hidden_size, n_hidden, output_size, activation, d_activation_dt):
         self.input_size = input_size
@@ -42,32 +42,49 @@ class MLP:
         self.biases = []
         self._init()
 
+    # Inicjalizacja wag i biasów
     def _init(self):
         for i in range(self.n_hidden + 1):
             if i == 0:
-                self.weights.append([[random.random() for _ in range(self.input_size)] for _ in range(self.hidden_size)])
-                self.biases.append([random.random() for _ in range(self.hidden_size)])
+                self.weights.append(
+                    [[random.random() for _ in range(self.input_size)]
+                        for _ in range(self.hidden_size)])
+                self.biases.append(
+                    [random.random() for _ in range(self.hidden_size)])
             elif i == self.n_hidden:
-                self.weights.append([[random.random() for _ in range(self.hidden_size)] for _ in range(self.output_size)])
-                self.biases.append([random.random() for _ in range(self.output_size)])
+                self.weights.append(
+                    [[random.random() for _ in range(self.hidden_size)]
+                        for _ in range(self.output_size)])
+                self.biases.append(
+                    [random.random() for _ in range(self.output_size)])
             else:
-                self.weights.append([[random.random() for _ in range(self.hidden_size)] for _ in range(self.hidden_size)])
-                self.biases.append([random.random() for _ in range(self.hidden_size)])
-        
+                self.weights.append(
+                    [[random.random() for _ in range(self.hidden_size)]
+                        for _ in range(self.hidden_size)])
+                self.biases.append(
+                    [random.random() for _ in range(self.hidden_size)])
+    
+    # Przepuszczanie danych przez sieć
     def forward(self, inputs):
         outputs = [inputs]
         for i in range(self.n_hidden + 1):
-            outputs.append([neuron(outputs[i], self.weights[i][j], self.biases[i][j], self.activation) for j in range(len(self.weights[i]))])
+            outputs.append(
+                [neuron(outputs[i], self.weights[i][j], self.biases[i][j], self.activation)
+                    for j in range(len(self.weights[i]))])
         return outputs
     
+    # Propagacja wsteczna
     def backward(self, outputs, targets, learning_rate):
         deltas = []
+        # Obliczenie gradientu
         for i in range(self.n_hidden + 1, 0, -1):
             if i == self.n_hidden + 1:
                 deltas.append([outputs[i][j] - targets[j] for j in range(len(outputs[i]))])
             else:
-                deltas.append([sum([deltas[-1][k] * self.weights[i][k][j] for k in range(len(deltas[-1]))]) * self.d_activation_dt(outputs[i][j]) for j in range(len(outputs[i]))])
-
+                deltas.append([sum(
+                    [deltas[-1][k] * self.weights[i][k][j] for k in range(len(deltas[-1]))])
+                         * self.d_activation_dt(outputs[i][j]) for j in range(len(outputs[i]))])
+        # Aktualizacja wag i biasów na podstawie gradientu
         deltas.reverse()
         for i in range(self.n_hidden + 1):
             for j in range(len(self.weights[i])):
@@ -75,6 +92,7 @@ class MLP:
                     self.weights[i][j][k] -= learning_rate * deltas[i][j] * outputs[i][k]
                 self.biases[i][j] -= learning_rate * deltas[i][j]
     
+    # Trenowanie sieci
     def fit(self, X, y, epochs, learning_rate, validation_data=None):
         self.weights = []
         self.biases = []
@@ -86,32 +104,37 @@ class MLP:
                 self.backward(outputs, y[i], learning_rate)
             if validation_data:
                 y_pred = self.predict(X)
-                history["train"].append(accuracy(np.argmax(y, axis=1), np.argmax(y_pred, axis=1)))
+                history["train"].append(
+                    accuracy(np.argmax(y, axis=1), np.argmax(y_pred, axis=1)))
                 X_val, y_val = validation_data
                 y_pred = self.predict(X_val)
-                history["val"].append(accuracy(np.argmax(y_val, axis=1), np.argmax(y_pred, axis=1)))
+                history["val"].append(
+                    accuracy(np.argmax(y_val, axis=1), np.argmax(y_pred, axis=1)))
         return history
     
+    # Predykcja
     def predict(self, X):
         return [self.forward(x)[-1] for x in X]
     
+    # Walidacja krzyżowa
     def cross_validate(self, folds, X, y, epochs, learning_rate):
-        fold_size = len(X) // folds
+        set_size = len(X) // folds
         scores = []
         X = list(X)
         y = list(y)
         for i in range(folds):
-            X_train = X[:i * fold_size] + X[(i + 1) * fold_size:]
-            y_train = y[:i * fold_size] + y[(i + 1) * fold_size:]
-            X_test = X[i * fold_size:(i + 1) * fold_size]
-            y_test = y[i * fold_size:(i + 1) * fold_size]
+            X_train = X[:i * set_size] + X[(i + 1) * set_size:]
+            y_train = y[:i * set_size] + y[(i + 1) * set_size:]
+            X_test = X[i * set_size:(i + 1) * set_size]
+            y_test = y[i * set_size:(i + 1) * set_size]
             self.fit(X_train, y_train, epochs, learning_rate)
             y_pred = self.predict(X_test)
-            scores.append(accuracy(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1)))
+            scores.append(
+                accuracy(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1)))
         return scores
             
 
-
+# Funkcja licząca dokładność
 def accuracy(y_target, y_pred):
     assert len(y_target) == len(y_pred)
     score = 0
